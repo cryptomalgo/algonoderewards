51871b874ada3191c238123fe0aa9623f64b6c25
cryptomalgo <205295302+cryptomalgo@users.noreply.github.com>
2025-03-06 17:24:18 +0100

    Replace wood with own NFD

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -32,7 +32,7 @@ export default function SearchBar() {
               id="query"
               name="query"
               type="text"
-              placeholder="woods.algo"
+              placeholder="noderewards.algo"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
               className="col-start-1 row-start-1 block w-full rounded-l-md bg-white py-1.5 pr-3 pl-10 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:pl-9 sm:text-lg/6"
