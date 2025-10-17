7f60e81a6c77b67db2c8a6d8e45a3b2071f166cc
cryptomalgo <205295302+cryptomalgo@users.noreply.github.com>
2025-10-17 15:59:30 +0200

    Add icon and hover on link

--- src/components/xgov-banner.tsx
+++ src/components/xgov-banner.tsx
@@ -1,5 +1,6 @@
 import { useApplication } from "@/hooks/useApplication";
 import { cn } from "@/lib/utils";
+import { ExternalLinkIcon } from "lucide-react";
 
 export default function XgovBanner() {
   const APP_ID = 3249061440;
@@ -18,15 +19,20 @@ export default function XgovBanner() {
     <div
       className={cn(
         {
-          "bg-indigo-600 text-white dark:bg-indigo-800": status === "voting",
-          "bg-gray-100 text-black dark:bg-slate-800 dark:text-white":
+          "bg-indigo-600 text-white hover:text-slate-300 dark:bg-indigo-800":
+            status === "voting",
+          "dark:over:text-indigo-800 bg-gray-100 text-black hover:text-indigo-500 dark:bg-slate-800 dark:text-white":
             status === "draft",
         },
         "flex items-center justify-center gap-x-6 px-6 py-2.5 sm:px-3.5",
       )}
     >
       <p className="text-sm/6">
-        <a href="https://xgov.algorand.co/proposal/3249061440" target="_blank">
+        <a
+          href="https://xgov.algorand.co/proposal/3249061440"
+          className="h inline-flex items-center gap-1 align-middle leading-none"
+          target="_blank"
+        >
           <strong className="font-semibold">xGov</strong>
           <svg
             viewBox="0 0 2 2"
@@ -38,7 +44,10 @@ export default function XgovBanner() {
           {status === "voting" &&
             "Voting is now open for our xGov proposal. Click here to vote!"}
           {status === "draft" &&
-            "A proposal to the Algorand xGov platform was submitted to fund this project. Click here to review it."}
+            "A proposal to the Algorand xGov platform was submitted to fund this project. Click here to review it"}
+          <span>
+            <ExternalLinkIcon className="relative top-[0.5px] size-3" />
+          </span>
         </a>
       </p>
     </div>
